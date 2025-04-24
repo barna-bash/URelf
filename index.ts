d07e9729a5db090597a0e504b4f8849457922333
Barni <barnabas.hajnal@gmail.com>
2025-04-24 23:29:24 +0200

    Migration scripts, dependencies, index.ts cleanup + health check api

--- index.ts
+++ index.ts
@@ -1,71 +1,22 @@
-import express from "express";
-import { ObjectId } from "mongodb";
-import URLController from "./controllers/urls.ts";
-import type { Url } from "./models/urls.ts";
+import express from 'express';
+import urlRoutes from './routes/url';
+import authRoutes from './routes/auth';
 
 const app = express();
 const port = 3000;
 
-const urlController = new URLController();
+// Global middleware
+app.use(express.json()); // For parsing JSON request bodies
 
+// Mount your route module under /url
+app.use('/urls/', urlRoutes);
+app.use('/auth/', authRoutes);
 
-// Get one url by id
-app.get("/url/:id", (req, res) => {
-  const { id } = req.params;
-  const urlId = new ObjectId(id);
-  urlController.getUrlById(urlId)
-    .then((urlDoc) => {
-      if (urlDoc) {
-        res.status(200).json(urlDoc);
-      } else {
-        res.status(404).json({ message: "Url not found" });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    });
+// Health check route
+app.get('/health-check', (_req, res) => {
+  res.send('Server is running!');
 });
-
-
-// Get all urls
-app.get("/urls", async (req, res) => {
-  const urlDocs = await urlController.getUrls();
-  const urls: Url[] = urlDocs.map(doc => ({
-    _id: doc._id,
-    from: doc.from,
-    to: doc.to,
-    clicks: doc.clicks
-  }));
-  res.json(urls);
-});
-
-
-// Update a url
-app.post("/url", express.json(), async (req, res) => {
-  const { from, to, description } = req.body;
-  const urlDoc = await urlController.addUrls({
-    from,
-    to,
-    description,
-  });
-  res.status(201).json(urlDoc);
-});
-
-
-// Delete a url
-app.delete("/url/:id", async (req, res) => {
-  const { id } = req.params;
-  const urlId = new ObjectId(id);
-  const urlDoc = await urlController.deleteUrl(urlId);
-  if (urlDoc) {
-    res.status(200).json({ message: "Url deleted successfully" });
-  } else {
-    res.status(404).json({ message: "Url not found" });
-  }
-});
-
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
